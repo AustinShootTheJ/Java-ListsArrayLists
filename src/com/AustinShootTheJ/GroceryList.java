c964691ec1f372b9ad2e28b956a7a5266a020bb0
AustinShootTheJ <austinjmccune@gmail.com>
2018-03-20 12:58:51 -0400

    Finished and notated the program.

--- src/com/AustinShootTheJ/GroceryList.java
+++ src/com/AustinShootTheJ/GroceryList.java
@@ -7,25 +7,75 @@ public class GroceryList {
     private ArrayList<String> groceryList = new ArrayList<String>();
 
 
+    // adds an item to the arraylist using the built in add() method of the ArrayList class.
     public void addGroceryItem(String item){
         groceryList.add(item);
     }
 
+    // getter for the arraylist
+    public ArrayList<String> getGroceryList() {
+        return groceryList;
+    }
+
+    // prints out the size of the arraylist and then loops through the list printing its contents.
     public void printGroceryList(){
         System.out.println("You have " + groceryList.size() + " items in your grocery list");
-        for(int i =0; i>groceryList.size(); i++){
+        for(int i = 0; i<groceryList.size(); i++){
             System.out.println((i+1) + ". " + groceryList.get(i));
         }
     }
 
-    public void modifyGroceryItem(int position, String newItem){
+    // takes two arguments the first is a current item input by the user and the second is an item that they wish to replace the first item with.
+    // the currentItem is passed to findItem which returns the index of an item in an arraylist. Since the function returns -1 if the item is
+    // not found we place a condition of >=0 for our check of position. Else the item was not found and not replacement can be made. If it
+    // is found we call the overloaded modifyGroceryItem which takes two arguments position and newItem and pass the index and our user input.
+    public void modifyGroceryItem(String currentItem, String newItem){
+        int position = findItem(currentItem);
+        if(position >= 0){
+            modifyGroceryItem(position,newItem);
+        }
+    }
+
+    // takes two arguments position and new item passed internally from the GroceryList class. Called from the above public method,
+    // this method takes a passed index and string and sets the item at the index to our new string using the set method of the ArrayList class.
+    private void modifyGroceryItem(int position, String newItem){
+
         groceryList.set(position,newItem);
         System.out.println("Grocery item " + (position + 1) + " has been modified.");
     }
 
+    // public method takes a string argument of an item input by our user and sets an int of position equal to the index result of
+    // findItem called on our entered item. findItem returns the index of an ArrayList item and returns -1 if the item is not present.
+    // we perform a check of position >= 0 to see if an item was found and if so we call the overloaded removeGroceryList internal method
+    // on our item to remove it from the ArrayList.
+    public void removeGroceryItem(String item){
+        int position = findItem(item);
+        if(position >= 0){
+            removeGroceryItem(position);
+        }
+    }
 
-    public void removeGroceryItem(int position){
-        String theItem = groceryList.get(position);
+    // uses the built in remove method of the ArrayList class to remove an item at the index received from the position argument.
+    private void removeGroceryItem(int position){
         groceryList.remove(position);
     }
+
+    // uses the built in indexOf() method of the ArrayList class to find the index of a String argument. Returns -1 if the
+    // string is not found.
+    private int findItem(String searchItem){
+    return groceryList.indexOf(searchItem);
+
+    }
+
+    // similar to the findItem method except it only validates that an item is on the list. It calls findItem and
+    // checks against the returned int. if the int is >= 0 then an item was found else it was not.
+    public boolean onFile(String searchItem){
+        int position = findItem(searchItem);
+        if(position >=0){
+            return true;
+        }
+        return false;
+
+    }
+
 }
--- src/com/AustinShootTheJ/Main.java
+++ src/com/AustinShootTheJ/Main.java
@@ -1,8 +1,119 @@
 package com.AustinShootTheJ;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 public class Main {
 
+    // creates a scanner for user input.
+    private static Scanner scanner = new Scanner(System.in);
+    // creates a groceryList object for us to work with.
+    private static GroceryList groceryList = new GroceryList();
+
     public static void main(String[] args) {
+       // a check for the user to exit our program.
+        boolean quit = false;
+        // user input to select a method.
+        int choice = 0;
+        // initial call of our instructions.
+        printInstructions();
+        // while loop calls our methods according to user input.
+        while(!quit){
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            // clears the scanner buffer.
+            scanner.nextLine();
+
+            switch(choice){
+                case 0:
+                    printInstructions();
+                    break;
+
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+
+                case 2:
+                    addItem();
+                    break;
+
+                case 3:
+                    modifyItem();
+                    break;
+
+                case 4:
+                    removeItem();
+                    break;
+
+                case 5:
+                    searchForItem();
+                    break;
+
+                case 6:
+                    processArrayList();
+                    break;
+
+                case 7:
+                    quit = true;
+                    break;
+
+            }
+
+        }
+    }
+    // prints options to the console.
+    public static void printInstructions(){
+        System.out.println("\n Press ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+
+    // calls the addGroceryItem method on our groceryList object.
+    public static void addItem(){
+        System.out.println("Please enter the grocery item: ");
+        groceryList.addGroceryItem(scanner.nextLine());
+    }
+    // takes input from the user and passes them into our modifyGroceryItem method on our GroceryList object.
+    public static void modifyItem(){
+        System.out.println("Enter item name: ");
+        String itemNo = scanner.nextLine();
+        System.out.println("Enter replacement item: ");
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryItem(itemNo, newItem);
+    }
+
+    // takes user input to pass into our removeGroceryItem method.
+    public static void removeItem(){
+        System.out.println("Enter item name: ");
+        String itemNo = scanner.nextLine();
+        groceryList.removeGroceryItem(itemNo);
+    }
+
+    // takes user input and passes it into the onFile method of our GroceryList object.
+    public static void searchForItem(){
+        System.out.println("Item to search for: ");
+        String searchItem = scanner.nextLine();
+        if(groceryList.onFile(searchItem)){
+            System.out.println("Found " + searchItem + " in our grocery list");
+
+        }else {
+            System.out.println(searchItem + " is not in the shopping list.");
+        }
+    }
+
+
+    // coppies our arrayList in various ways. 
+    public static void processArrayList(){
+        ArrayList<String> newArray = new ArrayList<String>();
+        newArray.addAll(groceryList.getGroceryList());
+
+        // The following converts an array list to an array!
+        //  String[] myArray = new String[groceryList.getGroceryList().size()];
+       //  myArray = groceryList.getGroceryList().toArray(myArray);
 
     }
+
 }
